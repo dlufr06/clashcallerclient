c2c1af60583ef0af9186b510cd0254a8079d5105
Daniel Lundberg <danlun@utb.vaxjo.se>
2016-01-12 11:32:53 +0100

    fixed index.html

--- public_html/src/js/index.js
+++ public_html/src/js/index.js
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function startWar(){
-    var nrOfBases = document.getElementById("nrOfBases").value;
-    if(typeof nrOfBases === "number"){
-        createWar(nrOfBases);
-    }
-}
+//function startWar(){
+//    var nrOfBases = document.getElementById("nrOfBases").value;
+//    if(typeof nrOfBases === "number"){
+//        createWar(nrOfBases);
+//    }
+//}
 
